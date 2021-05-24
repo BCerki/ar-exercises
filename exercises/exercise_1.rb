31e9f8a79d09a5390b6429a9227df58db29832bc
Brianna Cerkiewicz <briannac@shaw.ca>
2021-05-24 20:41:29 +0000

    remove variable assignment

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,17 +5,17 @@ puts "----------"
 
 # Your code goes below here ...
 
-store1 = Store.create(
+Store.create(
   annual_revenue: 300000,
   mens_apparel: true,
   womens_apparel: true)
 
-store2 = Store.create(
+Store.create(
   annual_revenue: 1260000,
   mens_apparel: false,
   womens_apparel: true)
 
-store3 = Store.create(
+Store.create(
   annual_revenue: 190000,
   mens_apparel: true,
   womens_apparel: false)
