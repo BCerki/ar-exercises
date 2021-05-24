5558f2f8bb00ced7d69a40467cc9dd7db3c14012
Brianna Cerkiewicz <briannac@shaw.ca>
2021-05-24 23:45:16 +0000

    complete exercise 8

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,8 +1,21 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :hourly_rate, numericality: {greater_than: 40}
   validates :hourly_rate, numericality: {less_than: 200}
   validates :store_id, presence: true
+
+  before_create :add_password
+
+  private
+  def generate_password
+    charset = Array('A'...'Z') + Array('a'...'z')
+    Array.new(8) {charset.sample}.join
+  end
+
+  def add_password
+    self.password = generate_password
+  end
 end
