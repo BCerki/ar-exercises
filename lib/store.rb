3304fe9582e59892e2c41230745a5ca245164b8b
Brianna Cerkiewicz <briannac@shaw.ca>
2021-05-25 00:08:48 +0000

    complete exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -4,12 +4,21 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
   validate :must_carry_apparel
 
+  before_destroy :safety_net
+
   def must_carry_apparel
     # abort "#{mens_apparel} #{womens_apparel}"
     tester_men = mens_apparel.nil? ? false : mens_apparel
     tester_women = womens_apparel.nil? ? false : womens_apparel
     if tester_men == false && tester_women == false
       errors.add(:mens_apparel, "or women's apparel must be carried")
+    end
+  end
+
+  def safety_net
+    if Employee.find_by(store_id: self.id)
+      puts "Deletion aborted. You cannot delete a store that has employees."
+      abort
     end
   end
 
